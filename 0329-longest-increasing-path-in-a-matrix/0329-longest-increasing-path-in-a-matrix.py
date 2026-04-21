df15ae720fdde51212a0570bf3c01eb4134d85cb
BiluBesto <2024503569@student.annauniv.edu>
2026-04-21 10:00:34 +0530

    Time: 136 ms (52.85%), Space: 36.4 MB (27.55%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -10,5 +10,12 @@ class Solution:
                 return dp[(r,c)]
             res=1
             res=max(res,1+dfs(r+1,c,matrix[r][c]))
-            res=max(res,1+dfs(r+1,c,matrix[r][c]))
+            res=max(res,1+dfs(r-1,c,matrix[r][c]))
+            res=max(res,1+dfs(r,c+1,matrix[r][c]))
+            res=max(res,1+dfs(r,c-1,matrix[r][c]))
             dp[(r,c)] = res
+            return res
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r,c,-1)
+        return max(dp.values())
