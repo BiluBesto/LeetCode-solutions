88aff3c0ee7715eb2ea9851c196dbcac1af67053
BiluBesto <2024503569@student.annauniv.edu>
2026-06-14 21:18:45 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.85%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0292-nim-game/0292-nim-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n>3 && n%4==0)
+        {
+            return false;
+        }
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
