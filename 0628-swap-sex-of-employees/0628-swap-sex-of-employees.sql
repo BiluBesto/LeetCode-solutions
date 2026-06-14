33d18bfa771c8bb918db305fd914d68ee4556734
BiluBesto <2024503569@student.annauniv.edu>
2026-06-14 22:53:25 +0530

    Time: 336 ms (75.26%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0628-swap-sex-of-employees/0628-swap-sex-of-employees.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+UPDATE Salary SET sex = 
+CASE WHEN sex='m' THEN 'f'
+ELSE 'm' END;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
