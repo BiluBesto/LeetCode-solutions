76c7c406e0535e26dbf680a42b5f3c0a2a7fb834
BiluBesto <2024503569@student.annauniv.edu>
2026-06-15 08:19:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (15.26%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string res = "";
+        char temp = ' ';
+        for(char c:s)
+        {   temp = c;
+            if(c-'Z'<=0 && (c=='A'||c=='B'||c=='C'||c=='D'||c=='E'||c=='F'||c=='G'||c=='H'||c=='I'||c=='J'||c=='K'||c=='L'||c=='M'||c=='N'||c=='O'||c=='P'||c=='Q'||c=='R'||c=='S'||c=='T'||c=='U'||c=='V'||c=='W'||c=='X'||c=='Y'||c=='Z'))
+            {
+                temp = c+32;
+            }
+            res+=temp;
+        }
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
