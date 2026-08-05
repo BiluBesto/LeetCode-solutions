49367112771f82908d3f830da27bdf8c44dd18a8
BiluBesto <2024503569@student.annauniv.edu>
2026-08-05 07:50:51 +0530

    Time: 3 ms (57.87%), Space: 19.4 MB (22.3%) - LeetHub - Time: O(k), Space: O(1)

--- 3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.py
+++ 3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.py
@@ -1,8 +1,8 @@
 class Solution:
     def sumOfGoodIntegers(self, n: int, k: int) -> int:
         sum=0
-        for x in range(1,n+k+1):
-             if abs(n-x)<=k and n&x==0:
+        for x in range(max(1,n-k),n+k+1):
+             if n&x==0:
                 sum+=x
         return sum
 
