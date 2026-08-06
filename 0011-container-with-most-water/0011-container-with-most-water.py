1eeb2b2f8dc07230f5ce540ae6caf47f396be421
BiluBesto <2024503569@student.annauniv.edu>
2026-08-06 12:34:12 +0530

    Time: 63 ms (26.09%), Space: 29.7 MB (14.24%) - LeetHub - Time: O(n), Space: O(1)

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -6,8 +6,12 @@ class Solution:
             area = (r-l)*min(height[l],height[r])
             res = max(res,area)
 
-            if height[l]>height[r]:
+            if height[l]>=height[r]:
                 r-=1
             else:
                 l+=1
         return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
