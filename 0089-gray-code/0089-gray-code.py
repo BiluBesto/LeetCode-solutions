3ac688a3bc03461c670d572f4536001d66cb7b42
BiluBesto <2024503569@student.annauniv.edu>
2026-08-19 07:16:03 +0530

    Time: 7 ms (70.07%), Space: 22.7 MB (13.43%) - LeetHub - Time: O(2^n), Space: O(2^n)

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,9 +1,6 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        res = []
-        for i in range(0,2**n):
-            res.append(i^(i//2))
-        return res
+        return [ i^(i>>1) for i in range(2**n)]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
