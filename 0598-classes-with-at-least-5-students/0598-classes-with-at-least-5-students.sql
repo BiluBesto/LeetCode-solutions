bdd978113e71df564183925f9abddf4fecb0bc82
BiluBesto <2024503569@student.annauniv.edu>
2026-08-22 12:43:13 +0530

    Time: 367 ms (87.89%), Space: 0B (100%) - LeetHub

---
+++ 0598-classes-with-at-least-5-students/0598-classes-with-at-least-5-students.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+SELECT class FROM Courses
+GROUP by class
+HAVING count(*)>=5
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
