b6e9fda12fd44431673c32ce1236c69621f517ba
BiluBesto <2024503569@student.annauniv.edu>
2026-08-22 10:22:45 +0530

    Time: 563 ms (84.41%), Space: 0B (100%) - LeetHub

---
+++ 0193-customers-who-never-order/0193-customers-who-never-order.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+Select name Customers FROM Customers WHERE id not in (Select customerId from orders)
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
