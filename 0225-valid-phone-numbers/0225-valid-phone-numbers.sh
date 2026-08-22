1a645f90884e2b435de1d044fc91370d9196212b
BiluBesto <2024503569@student.annauniv.edu>
2026-08-22 10:30:29 +0530

    Time: 58 ms (93.52%), Space: 3.5 MB (68.83%) - LeetHub

---
+++ 0225-valid-phone-numbers/0225-valid-phone-numbers.sh
@@ -0,0 +1,6 @@
+# Read from the file file.txt and output all valid phone numbers to stdout.
+grep -E '^((\([0-9]{3}\) [0-9]{3}-[0-9]{4})|([0-9]{3}-[0-9]{3}-[0-9]{4}))$' file.txt
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
