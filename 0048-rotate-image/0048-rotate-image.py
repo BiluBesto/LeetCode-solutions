9cc0158843beb901f5820d88fe3fe62b01a8778a
BiluBesto <2024503569@student.annauniv.edu>
2026-08-24 17:13:17 +0530

    Time: 0 ms (100%), Space: 19.2 MB (68.65%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        for i in range(len(matrix)):
+            for j in range(i,len(matrix[0])):
+                matrix[i][j],matrix[j][i]= matrix[j][i],matrix[i][j]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])//2):
+                matrix[i][j],matrix[i][len(matrix[0])-j-1] = matrix[i][len(matrix[0])-j-1], matrix[i][j]
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
