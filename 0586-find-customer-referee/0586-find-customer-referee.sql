ca3d538a625dfae86d98276053070098673740ef
BiluBesto <2024503569@student.annauniv.edu>
2026-08-25 15:15:45 +0530

    Time: 556 ms (43.96%), Space: 0B (100%) - LeetHub

---
+++ 0586-find-customer-referee/0586-find-customer-referee.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+SELECT name from Customer where referee_id!=2 or referee_id is null;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
