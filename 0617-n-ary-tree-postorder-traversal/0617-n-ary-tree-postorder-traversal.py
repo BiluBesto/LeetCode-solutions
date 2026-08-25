70574cead7106316d98b73bd9388a96bcd2fa35e
BiluBesto <2024503569@student.annauniv.edu>
2026-08-25 15:21:32 +0530

    Time: 54 ms (61.57%), Space: 21 MB (22.74%) - LeetHub

---
+++ 0617-n-ary-tree-postorder-traversal/0617-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        res = []
+        def dfs(root):
+            if root is None:
+                return
+            
+            for child in root.children:
+                dfs(child)
+
+            res.append(root.val)
+        dfs(root)
+
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
