fe8671d6cf2fe35338b17ae29b70ae7a524602b4
BiluBesto <2024503569@student.annauniv.edu>
2026-08-26 22:24:39 +0530

    nd(

---
+++ 1211-unique-number-of-occurrences/1211-unique-number-of-occurrences.py
@@ -0,0 +1,22 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        arr.sort()
+        v = []
+        i = 0
+        while i < len(arr):
+            cnt = 1
+            while i + 1< len(arr) and arr[i]==arr[i+1]:
+                cnt+=1
+                i+=1
+            v.append(cnt)
+            i+=1
+        v.sort()
+
+        for i in range(1,len(v)):
+            if v[i]==v[i-1]:
+                return False
+        return True
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
