75eb8e486439db28d82f0d443242ac09768e790d
BiluBesto <2024503569@student.annauniv.edu>
2026-08-26 21:42:13 +0530

    Time: 549 ms (57.75%), Space: 0B (100%) - LeetHub

---
+++ 1078-project-employees-i/1078-project-employees-i.sql
@@ -0,0 +1,14 @@
+/* Write your PL/SQL query statement below */
+SELECT project_id,
+ROUND(SUM(experience_years)/COUNT(project_id),2) AS average_years
+FROM (
+    SELECT p.project_id,p.employee_id, e.experience_years
+    FROM Project p
+    LEFT JOIN Employee e
+    ON p.employee_id = e.employee_id
+)
+GROUP BY project_id
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
