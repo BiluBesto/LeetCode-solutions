98676cafbbb50b9b7278f9670ef8251952177e9d
BiluBesto <2024503569@student.annauniv.edu>
2026-08-26 19:54:00 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61.81%) - LeetHub

---
+++ 1018-complement-of-base-10-integer/1018-complement-of-base-10-integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        mask = n|1
+        for i in range(0,5):
+            mask |= mask>>(1<<i)
+        return n^mask
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
