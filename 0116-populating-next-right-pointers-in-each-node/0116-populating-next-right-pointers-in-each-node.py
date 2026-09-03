22892db8d3528087540b95e1b69da1f0c925e8e0
BiluBesto <2024503569@student.annauniv.edu>
2026-09-03 20:27:39 +0530

    Time: 61 ms (32.66%), Space: 20.8 MB (17.82%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,46 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if root is None:
+            return root
+        root.next = None
+        q = deque()
+        q.append(root.left)
+        q.append(root.right)
+        while q:
+            p = deque()
+            while q:
+                p.append(q.popleft())
+            l,m,n = None,None,None
+            while p:
+                l = p.popleft()
+                if l is None:
+                    while q:
+                        x=q.popleft()
+                    break
+                m = p.popleft()
+                q.append(l.left)
+                q.append(l.right)
+                q.append(m.left)
+                q.append(m.right)
+                if n is not None:
+                    n.next = l
+                l.next = m
+                n = m
+            if m is not None:
+                m.next = None
+        return root
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
