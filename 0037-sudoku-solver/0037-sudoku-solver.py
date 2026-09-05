a0242f1eca9c3f24a4a48a470d660558e8536ea1
BiluBesto <2024503569@student.annauniv.edu>
2026-09-05 07:47:51 +0530

    Time: 2501 ms (32.72%), Space: 19.7 MB (16.13%) - LeetHub - Time: O(9^(81)), Space: O(1)

---
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -0,0 +1,48 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        used_row = [set() for _ in range(9)]
+        used_col = [set() for _ in range(9)]
+        used_box = [set() for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]!='.':
+                    val = int(board[i][j])
+                    used_row[i].add(val)
+                    used_col[j].add(val)
+                    box_id = (i//3)*3 + j//3
+                    used_box[box_id].add(val)
+        def is_valid(i,j,val):
+            box_id = (i//3)*3 + (j//3)
+            return val not in used_box[box_id] and val not in used_row[i] and val not in used_col[j]
+        
+        def dfs(i,j):
+            if i == 9:
+                return True
+            nexti,nextj = 0,0
+            if j<8:
+                nexti,nextj = i,j+1
+            else:
+                nexti,nextj = i+1, 0
+            if board[i][j]!='.':
+                return dfs(nexti,nextj)
+            box_id = (i//3)*3 + (j//3)
+            for x in range(1,10):
+                if is_valid(i,j,x):
+                    board[i][j]=str(x)
+                    used_row[i].add(x)
+                    used_col[j].add(x)
+                    used_box[box_id].add(x)
+                    if dfs(nexti, nextj):
+                        return True
+                    board[i][j]='.'
+                    used_row[i].remove(x)
+                    used_col[j].remove(x)
+                    used_box[box_id].remove(x)
+            return False
+        dfs(0,0)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
