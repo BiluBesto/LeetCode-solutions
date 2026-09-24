d1823cf6489e0f8e7d6308c9176226549808399e
BiluBesto <2024503569@student.annauniv.edu>
2026-09-24 13:36:25 +0530

    Time: 13 ms (31.09%), Space: 37.9 MB (57.34%) - LeetHub - Time: O(n + m), Space: O(1)

---
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getCommon(self, nums1: list[int], nums2: list[int]) -> int:
+        i = 0
+        j = 0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i]>nums2[j]:
+                j+=1
+            else:
+                i+=1
+        return -1
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
