f0bf94022870cb2c394140bd3acee6be2963f471
BiluBesto <2024503569@student.annauniv.edu>
2026-09-24 13:40:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (26.48%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        st = set(word)
+        count = 0
+        for i in range(26):
+            lower = chr(ord('a')+i)
+            upper = chr(ord('A')+i)
+            if lower in st and upper in st:
+                count+=1
+        return count
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
